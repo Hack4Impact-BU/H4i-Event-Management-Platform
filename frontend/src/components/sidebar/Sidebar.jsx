0a68cce6213ba623f5bc22785d9f8916fdca4643
Patrick Fish <patrickfish10@icloud.com>
2025-04-01 18:13:04 -0400

    globe style changed

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -154,7 +154,7 @@ const Sidebar = ({ selectedEvent, closeSidebar, onUpdateEvent }) => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          'summary': `🌍 ${selectedEvent.title}`,
+          'summary': `🌎 ${selectedEvent.title}`,
           'location': selectedEvent.location,
           'description': selectedEvent.description,
           'start': {
