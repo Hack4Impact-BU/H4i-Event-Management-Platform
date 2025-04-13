0e3c73e2d3912f8667e150cc579b94423a2dc4e2
Sadid Ethun <ethun2004@gmail.com>
2025-04-13 18:45:53 -0400

    added navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,69 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/login/login';
-import Home from './pages/home/home';
-import './App.css';
+import React, { useState } from "react";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from "react-router-dom";
+import Login from "./pages/login/login";
+import Home from "./pages/home/home";
+import Finances from "./pages/finances/finances";
+import "./App.css";
 
 function App() {
+	const [isAuthenticated, setIsAuthenticated] = useState(() => {
+		return !!sessionStorage.getItem("authToken");
+	});
 
-  const [isAuthenticated, setIsAuthenticated] = useState(() => {
-    return !!sessionStorage.getItem('authToken');
-  });
+	function ErrorBoundary() {
+		const localLink = window.location.href.substring(
+			window.location.href.lastIndexOf("/")
+		);
+		return (
+			<>
+				<h1 style={{ textAlign: "center", marginTop: "5rem" }}>
+					Error 404: Page Not Found
+				</h1>
+				<h2 style={{ textAlign: "center", marginBottom: "5rem" }}>
+					The requested URL {localLink} was not found on this server.
+				</h2>
+			</>
+		);
+	}
 
-  function ErrorBoundary() {
-    const localLink = window.location.href.substring(window.location.href.lastIndexOf('/'));
-    return (
-      <>
-        <h1 style={{ textAlign: 'center', marginTop: '5rem' }}>
-          Error 404: Page Not Found
-        </h1>
-        <h2 style={{ textAlign: 'center', marginBottom: '5rem' }}>
-          The requested URL {localLink} was not found on this server.
-        </h2>
-      </>
-    );
-  }
-
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={isAuthenticated ? <Navigate to="/home" replace /> : <Login setIsAuthenticated={setIsAuthenticated} />} />
-        <Route path="/home" element={isAuthenticated ? <Home /> : <Navigate to="/" replace />} />
-        <Route path="*" element={<ErrorBoundary />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route
+					path="/"
+					element={
+						isAuthenticated ? (
+							<Navigate to="/home" replace />
+						) : (
+							<Login setIsAuthenticated={setIsAuthenticated} />
+						)
+					}
+				/>
+				<Route
+					path="/home"
+					element={
+						isAuthenticated ? <Home /> : <Navigate to="/" replace />
+					}
+				/>
+				<Route
+					path="/finances"
+					element={
+						isAuthenticated ? (
+							<Finances />
+						) : (
+							<Navigate to="/" replace />
+						)
+					}
+				/>
+				<Route path="*" element={<ErrorBoundary />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,35 +1,43 @@
-import React from 'react';
-import { Box, IconButton, Button, Typography, Link } from '@mui/material';
-import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
-import AttachMoneyOutlinedIcon from '@mui/icons-material/AttachMoneyOutlined';
-import EventNoteOutlinedIcon from '@mui/icons-material/EventNoteOutlined';
-import WatchLaterOutlinedIcon from '@mui/icons-material/WatchLaterOutlined';
-import H4iLogo from '../../assets/h4i_removed_bg.png';
-import './Navbar.css';
+import React from "react";
+import { Box, IconButton, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import AttachMoneyOutlinedIcon from "@mui/icons-material/AttachMoneyOutlined";
+import EventNoteOutlinedIcon from "@mui/icons-material/EventNoteOutlined";
+import H4iLogo from "../../assets/h4i_removed_bg.png";
+import "./Navbar.css";
 
 const NavBar = () => {
-    return (
-        <Box className="navbar">
-            <img id="h4i_logo" src={H4iLogo} />
-            <Box className="pages">
-                <Link id="events_box" href="#" underline="hover">
-                    <EventNoteOutlinedIcon id="event_icon" />
-                    <Typography id="events" variant='h5'>Events</Typography>
-                </Link>
-                <Link id="finances_box" href="#" underline="hover" variant='h4'>
-                    <AttachMoneyOutlinedIcon id="finance_icon" />
-                    <Typography id="finances" variant='h5'>Finances</Typography>
-                </Link>
-                <Link id="budget_simulation_box" href="#" underline="hover" variant='h4'>
-                    <WatchLaterOutlinedIcon id="budget_icon" />
-                    <Typography id="budget_simulation" variant='h5'>Budget Simulation</Typography>
-                </Link>
-            </Box>
-            <IconButton>
-                <SettingsOutlinedIcon id="setting_button" />
-            </IconButton>
-        </Box>
-    )
-}
+	return (
+		<Box className="navbar">
+			<img id="h4i_logo" src={H4iLogo} alt="H4i Logo" />
+			<Box className="pages">
+				<RouterLink
+					id="events_box"
+					to="/home"
+					style={{ textDecoration: "none" }}
+				>
+					<EventNoteOutlinedIcon id="event_icon" />
+					<Typography id="events" variant="h5">
+						Events
+					</Typography>
+				</RouterLink>
+				<RouterLink
+					id="finances_box"
+					to="/finances"
+					style={{ textDecoration: "none" }}
+				>
+					<AttachMoneyOutlinedIcon id="finance_icon" />
+					<Typography id="finances" variant="h5">
+						Finances
+					</Typography>
+				</RouterLink>
+			</Box>
+			<IconButton id="settings_icon">
+				<SettingsOutlinedIcon id="setting_button" />
+			</IconButton>
+		</Box>
+	);
+};
 
 export default NavBar;
