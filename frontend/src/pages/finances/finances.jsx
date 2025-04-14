8e1acc56d3f24a55549e1640e865bb665aae9830
Patrick Fish <patrickfish10@icloud.com>
2025-04-13 21:30:37 -0400

    data-table and pie chart (non-functional)

--- frontend/src/pages/finances/finances.jsx
+++ frontend/src/pages/finances/finances.jsx
@@ -1,5 +1,34 @@
-import NavBar from "../../components/navbar/navbar";
+import React, { useState, useEffect, useRef } from 'react';
+import { PieChart } from '@mui/x-charts/PieChart';
+import NavBar from "../../components/navbar/Navbar";
+import FinanceTable from "../../components/financeTable/financeTable";
+import "./finances.css";
 
 export default function Finances() {
-	return <NavBar />;
+	return (
+		<>
+			<div>
+				<NavBar />
+			</div>
+			<div className="finance_information_container">
+			<PieChart
+				height={300}
+				series={[
+				{
+					data: [
+						{ value: 15, label: 'A' },
+						{ value: 30, label: 'B' },
+						{ value: 10, label: 'C' },
+						{ value: 60, label: 'D' },
+					],
+					innerRadius: "75px",
+					arcLabel: (params) => params.label ?? '',
+					arcLabelMinAngle: 20,
+				},
+				]}
+			/>
+				<FinanceTable />
+			</div>
+		</>	
+	);
 }
